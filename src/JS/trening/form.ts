fd3480374dc05f88c4e79598702fe3a9cf40a73f
Ole M <olesnoen@gmail.com>
2024-05-06 10:47:47 +0200

    fix

--- src/JS/trening/form.ts
+++ src/JS/trening/form.ts
@@ -25,7 +25,7 @@ export const programForm = (prog: Exercise[]) => {
         button.classList.add("form_exercise_button");
         button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>`;
         button.setAttribute("type", "button");
-        button.addEventListener("click", (e) => {
+        button.addEventListener("click", () => {
             addExerciseToObject(i.toString());
             button.style.backgroundColor = "rgb(146, 222, 146)";
         });
